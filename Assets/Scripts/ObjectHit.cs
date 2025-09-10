818b9c1c97ab4455b0c0bda890318d474aa0b1b6
dmccowan <mccowa206@morrisville.edu>
2025-09-10 15:38:14 -0400

    Video08: GetComponents

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
+        GetComponent<MeshRenderer>().material.color = Color.darkRed;
         Debug.Log(gameObject.name + " was hit by " + collision.gameObject.name);
     }
 }
