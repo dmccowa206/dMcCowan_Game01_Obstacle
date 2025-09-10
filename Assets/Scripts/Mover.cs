8352c3ff5ce517c381c2c50f28b17cab5236a3f7
dmccowan <mccowa206@morrisville.edu>
2025-09-10 15:29:11 -0400

    Video 06: Methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 8f;
+    float yValue = 0f;
     void Start()
     {
+        PrintInstruction();
     }
     void Update()
     {
+        MovePlayer();
+    }
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game.");
+        Debug.Log("Use WASD or arrow keys to move");
+        Debug.Log("Don't bump into things");
+    }
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
     }
